129247cde0aa4c74e9ebc86b93e4980e52255f7f
Vasiliy Soshnikov <dedok.mad@gmail.com>
2016-06-13 16:59:49 +0300

    refactoring

--- examples/in_device.lua
+++ examples/in_device.lua
@@ -3,51 +3,15 @@
 -- Includes
 local fiber = require('fiber')
 local mraa = require('mraa')
+local yaml = require('yaml')
+local log = require('log')
 
+-- Device cfg
 local cfg = {
   device_name = 'edison_1',
 }
 
--- Shortcuts
-local device = {
-  sensors = {}
-}
-
-local function work()
-
-  -- Work helpers
-  local function get_temperature()
-    local B = 3975
-    local a = device.sensors.temperature:read()
-    local resistance = (1023-a)*10000.0/a
-    return 1/(math.log(resistance/10000.0)/B+1/298.15)-273.15
-  end
-
-  -- Shortcuts
-  local sensors_timeline = device.sensors_timeline
-  local st_ttl = sensors_timeline.index.ttl;
-
-  while true do
-
-    local current_time = fiber.time()
-
-    -- drain olds
-    local tuples = sensors_timeline
-    for _, tuple in st_ttl:select{current_time, {iterator = 'LT'}} do
-      local id = tuple:unpack()
-      st_ttl:delete{id}
-    end
-
-    sensors_timeline:auto_increment{{
-      temperature = get_temperature(),
-      ttl = current_time + 60
-    }}
-
-    fiber.sleep(1)
-  end
-end
-
--- Init
+-- Init master - master [[
 box.cfg {
   listen = '0.0.0.0:3301',
   log_level = 5,
@@ -59,23 +23,142 @@ box.cfg {
   -- FIXME Add here your host
   replication_source = { '192.168.1.37:3301' }
 }
+-- ]]
 
+--
+-- Abstract helpers [[
+local function test_max_value(self, sensor_data)
+  local name = self.name
+  local tuple = settings:get{name}
+  if not tuple then
+    settings:insert{name, {max = self.default_max, alert = false}}
+    tuple = settings:get{name}
+  end
+  local _, settings = settings[1]:unpack()
+  if sensor_data[self.name] > settings.max then
+    settings.alert = true
+    settings:replace{name, settings}
+  end
+  return settings.alert
+end
+
+local function aio_read(self)
+  return self.io:read()
+end
+-- ]]
+
+--
+-- Device object
 device =  {
-  message_bus = box.space[cfg.device .. '_mbus'],
+
+  -- Spaces references
+  message_bus = box.space[cfg.device_name .. '_mbus'],
   settings = box.space[cfg.device_name .. '_settings'],
   sensors_timeline = box.space[cfg.device_name .. '_sensors_timeline'],
 
+  -- Sensors e.g input sensors
   sensors = {
-    light = mraa.Aio(0),
-    temperature = mraa.Aio(1),
-    sound = mraa.Aio(2),
+
+    light = {
+      io = mraa.Aio(0),
+      name = 'light',
+      default_max = 100,
+
+      get_data = aio_read,
+      test_max = test_max_value
+    },
+
+    temperature = {
+      io = mraa.Aio(1),
+      name = 'temperature',
+      default_max = 30,
+
+      get_data = function(self)
+        local B = 3975
+        local a = self.io:read()
+        local resistance = (1023-a)*10000.0/a
+        return math.floor(1/(math.log(resistance/10000.0)/B+1/298.15)-273.15)
+      end,
+
+      test_max = test_max_value
+    },
+
+    sound = {
+      io = mraa.Aio(2),
+      name = 'sound',
+      default_max = 100,
+
+      get_data = aio_read,
+      test_max = test_max_value
+    }
+  },
+
+  -- Alerts e.g. output devices
+  alert = {
+
+    initialised = false,
+
     diod_light = mraa.Gpio(4),
-    buzzer = mraa.Gpio(8)
+    buzzer = mraa.Gpio(8),
+
+    init_once = function(self)
+      if not initialised then
+        self.diod_light:dir(mraa.DIR_OUT)
+        self.buzzer:dir(mraa.DIR_OUT)
+        initialised = true
+      end
+    end,
+
+    alert = function(self, state)
+      self:init_once()
+    end
   }
 }
+-- ]]
 
-device.sensors.diod_light:dir(mraa.DIR_OUT)
-device.sensors.buzzer:dir(mraa.DIR_OUT)
+local function main_work()
 
--- Start
-fiber.create(work)
+  -- Shortcuts
+  local settings = device.settings
+  local sensors_timeline = device.sensors_timeline
+  local st_ttl = sensors_timeline.index.ttl
+  local sensors = device.sensors
+  local alert = device.alert
+
+  -- Work loop [[
+  local ticks = 0
+
+  while true do
+
+    log.error('[+] cycle ...')
+
+    -- Cleanup an old sensors data
+    if ticks > 10 then
+      ticks = 0
+        -- drain olds
+        for _, tuple in pairs(st_ttl:select{}) do
+                print(yaml.encode(tuple))
+                local id = tuple:unpack()
+                sensors_timeline:delete{id}
+        end
+    end
+
+    -- Collect data from IN sensors
+    local sensors_data = {}
+    for _, sensor in pairs(sensors) do
+      local name = _
+      local value = sensor:get_data()
+      alert:alert(sensor:test_max(value))
+      sensors_data[name] = value
+    end
+
+    sensors_timeline:auto_increment{ticks, sensors_data}
+
+    ticks = ticks + 1
+    fiber.sleep(1)
+  end
+  -- Work loop ]]
+end
+
+-- Start async
+fiber.create(main_work)
